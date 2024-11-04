4389333678bae2dc538832a9709ffa19d81fac36
Shishir Dey <shishirdey241097@gmail.com>
2024-11-04 22:52:26 +0530

    Create a new React project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>Hello, world!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
