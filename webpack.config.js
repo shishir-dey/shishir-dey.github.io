a86afaf5046d6fbed03b747e1e36575c348935f6
Shishir Dey <shishirdey241097@gmail.com>
2025-12-29 14:40:02 +0530

    Copy BingSiteAuth.xml to build root for Bing Webmaster verification

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,10 @@ module.exports = {
         {
           from: path.resolve(__dirname, './public/logo.png'),
           to: path.resolve(__dirname, 'build')
+        },
+        {
+          from: path.resolve(__dirname, './public/BingSiteAuth.xml'),
+          to: path.resolve(__dirname, 'build')
         }
       ]
     })
